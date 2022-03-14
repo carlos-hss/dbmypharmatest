970514abb2c6ae6c21df1189aec03016712a0b13
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2022-03-14 16:28:44 -0300

    fix(js): Trying To Fix the Heroku Integration

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-    "mongodb+srv://dev:OjGhKZbDWwmKtm5f@cluster0.m5gh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    process.env.DATABASE_URL ||
+        "mongodb+srv://dev:OjGhKZbDWwmKtm5f@cluster0.m5gh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
         useNewUrlParser: true,
     }
